c84549efe2a00a2185e7adda63eb5bfb3b0e2a83
Iker <ikernoya4@gmail.com>
2021-03-23 21:11:42 -0300

    Created basic movement for fps controller

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed;
+    [SerializeField] float groundDistance;
+    [SerializeField] float jumpHeight;
+    [SerializeField] Camera cam;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+
+    float gravity = -9.81f * 2;
+
+    CharacterController controller;
+
+    Vector3 movement;
+    Vector3 velocity;
+
+    bool isGrounded;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        movement = transform.right * x + transform.forward * z;
+
+        controller.Move(movement * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // jump formula: result = sqrt( h * -2 * g)
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
