868daacbd147fd051e430365927c21704bf175d4
Iker <ikernoya4@gmail.com>
2021-04-17 15:34:48 -0300

    Added option to toggle sprint

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -6,22 +6,27 @@ public class FPSController : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float sprintSpeed;
+    [SerializeField] float crouchSpeed;
     [SerializeField] float groundDistance;
     [SerializeField] float jumpHeight;
     [SerializeField] Camera cam;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
 
+    public bool crouchToggle;
+    public bool sprintToggle;
+
+    CharacterController controller;
 
     float gravity = -9.81f * 2;
     float yNegativeVelocity = -2;
-
-    CharacterController controller;
 
     Vector3 movement;
     Vector3 velocity;
 
     bool isGrounded;
+    bool isSprinting;
+    bool isCrouched;
 
     void Start()
     {
@@ -47,17 +52,64 @@ public class FPSController : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
 
         Inputs();
+        Movement();
     }
     void Inputs()
     {
         //movement
-        if (Input.GetKey(KeyCode.LeftShift))
-            controller.Move(movement * sprintSpeed * Time.deltaTime);
+        if (!sprintToggle)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isSprinting = true;
+                isCrouched = false;
+            }
+            else
+                isSprinting = false;
+        }
         else
-            controller.Move(movement * speed * Time.deltaTime);
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting)
+            {
+                isSprinting = true;
+                isCrouched = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftShift) && isSprinting)
+                isSprinting = false;
+        }
+
+        if(!crouchToggle)
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                isCrouched = true;
+                isSprinting = false;
+            }
+            else
+                isCrouched = false;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouched)
+            {
+                isCrouched = true;
+                isSprinting = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouched)
+                isCrouched = false;
+        }
 
         //jump
         if (Input.GetButtonDown("Jump") && isGrounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // jump formula: result = sqrt( h * -2 * g)
+    }
+    void Movement()
+    {
+        if(isSprinting)
+            controller.Move(movement * sprintSpeed * Time.deltaTime);
+        if (isCrouched)
+            controller.Move(movement * crouchSpeed * Time.deltaTime);
+        if (!isSprinting && !isCrouched)
+            controller.Move(movement * speed * Time.deltaTime);
     }
 }
