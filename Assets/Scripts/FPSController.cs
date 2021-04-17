7430e9843895779b8e65e3ce687b2149efa2bb73
Iker <ikernoya4@gmail.com>
2021-04-17 15:03:49 -0300

    Optimized playerController code

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -12,7 +12,9 @@ public class FPSController : MonoBehaviour
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
 
+
     float gravity = -9.81f * 2;
+    float yNegativeVelocity = -2;
 
     CharacterController controller;
 
@@ -31,27 +33,31 @@ public class FPSController : MonoBehaviour
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
+            velocity.y = yNegativeVelocity;
+        
 
+        //movement + sprint
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         movement = transform.right * x + transform.forward * z;
 
-        if(Input.GetKey(KeyCode.LeftShift))
+        //jump + artificial gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        Inputs();
+    }
+    void Inputs()
+    {
+        //movement
+        if (Input.GetKey(KeyCode.LeftShift))
             controller.Move(movement * sprintSpeed * Time.deltaTime);
         else
             controller.Move(movement * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
+        //jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // jump formula: result = sqrt( h * -2 * g)
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
     }
 }
