c10018a6b976bbf7d39d82d87f76418a953fe449
fermingimpel <fermingimpel@gmail.com>
2021-06-11 18:59:53 -0300

    Changes in the Crouch

--- Assets/Scripts/Player/FPSController.cs
+++ Assets/Scripts/Player/FPSController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSController : MonoBehaviour
-{
+public class FPSController : MonoBehaviour {
     [SerializeField] float speed;
     [SerializeField] float sprintSpeed;
     [SerializeField] float crouchMovementSpeed;
@@ -40,21 +39,19 @@ public class FPSController : MonoBehaviour
     bool isSprinting;
     bool isCrouched;
 
-    void Start()
-    {
+    void Start() {
         controller = GetComponent<CharacterController>();
         hpController = GetComponent<HPController>();
         cam = Camera.main.transform;
     }
 
-    void Update()
-    {
+    void Update() {
         if (!hpController.GetIsAlive() && hpController != null)
             return;
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
             velocity.y = yNegativeVelocity;
 
         //movement + sprint
@@ -72,33 +69,29 @@ public class FPSController : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-        
+
 
         Inputs();
         Movement();
         Crouch();
         HeadBobbing();
     }
-    void Inputs()
-    {
+    void Inputs() {
         //movement
-        if (isGrounded)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
+        if (isGrounded) {
+            if (Input.GetKey(KeyCode.LeftShift)) {
                 isSprinting = true;
                 isCrouched = false;
             }
             else
                 isSprinting = false;
 
-            if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouched)
-            {
+            if (Input.GetKey(KeyCode.LeftControl)) {
                 isCrouched = true;
                 isSprinting = false;
             }
-            else if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouched)
-                    isCrouched = false;
+            else
+                isCrouched = false;
         }
 
         //jump
@@ -106,22 +99,20 @@ public class FPSController : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // jump formula: result = sqrt( h * -2 * g)
 
 
-//#if UNITY_EDITOR
-//        if (Input.GetKeyDown(KeyCode.Escape))
-//            UnityEditor.EditorApplication.isPlaying = false;
-//#endif 
+        //#if UNITY_EDITOR
+        //        if (Input.GetKeyDown(KeyCode.Escape))
+        //            UnityEditor.EditorApplication.isPlaying = false;
+        //#endif 
     }
-    void Movement()
-    {
-        if(isSprinting)
+    void Movement() {
+        if (isSprinting)
             controller.Move(movement * sprintSpeed * Time.deltaTime);
         if (isCrouched)
             controller.Move(movement * crouchMovementSpeed * Time.deltaTime);
         if (!isSprinting && !isCrouched)
             controller.Move(movement * speed * Time.deltaTime);
     }
-    void Crouch()
-    {
+    void Crouch() {
         float height = cam.position.y;
         if (isCrouched && height > CrouchedHead.position.y + 0.5f)
             height -= Time.deltaTime * crouchSpeed;
@@ -130,47 +121,39 @@ public class FPSController : MonoBehaviour
         cam.position = new Vector3(cam.position.x, height, cam.position.z);
     }
 
-    void HeadBobbing()
-    {
+    void HeadBobbing() {
         Vector3 newHeadPosition;
         if (!isCrouched)
             newHeadPosition = Head.position + CalculateHeadBobOffset(timeWalking);
         else
             newHeadPosition = CrouchedHead.position + CalculateHeadBobOffset(timeWalking);
 
-        if (isSprinting && !isCrouched)
-        {
+        if (isSprinting && !isCrouched) {
             hBobFrequency = sprintingHeadBobFrequency;
             hBobVerticalAmplitude = standingHeadBobVerticalAmplitude;
         }
-        else if (!isSprinting && isCrouched)
-        {
+        else if (!isSprinting && isCrouched) {
             hBobFrequency = walkingHeadBobFrequency;
             hBobVerticalAmplitude = crouchingHeadVerticalAmplitude;
         }
-        else
-        {
+        else {
             hBobFrequency = walkingHeadBobFrequency;
             hBobVerticalAmplitude = standingHeadBobVerticalAmplitude;
         }
 
-        if (isGrounded)
-        {
+        if (isGrounded) {
             cam.position = Vector3.Lerp(cam.position, newHeadPosition, headBobbingIntensity);
-            if ((cam.position - newHeadPosition).magnitude <= axisDifference)
-            {
+            if ((cam.position - newHeadPosition).magnitude <= axisDifference) {
                 cam.position = newHeadPosition;
             }
         }
 
 
     }
-    Vector3 CalculateHeadBobOffset(float value)
-    {
+    Vector3 CalculateHeadBobOffset(float value) {
         float movement;
         Vector3 offset = Vector3.zero;
-        if (value > 0)
-        {
+        if (value > 0) {
             movement = Mathf.Sin(value * hBobFrequency * 2) * hBobVerticalAmplitude;
             if (!isCrouched)
                 offset = Head.transform.up * movement;
